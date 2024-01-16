871e57564bd624a0596526e441f975141cb12612
Eswar S Sethu <63389431+Eswar-S-Sethu@users.noreply.github.com>
2024-01-16 12:29:39 +1100

    added basic socket functionality

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { logErrors, errorHandler } = require('./utils/errorHandler.js')
 let userRoutes = require('./routes/user.routes.js');
 const path = require('path');
 const app = express()
-const port = 3000 | process.env.port
+const port = 3000 || process.env.port
 
 //database connection
 require('./utils/dbConnection')
@@ -24,6 +24,35 @@ app.use('/api/user', userRoutes)
 //error handling
 app.use(logErrors)
 app.use(errorHandler)
+// ------------------------------------------------------------------------------------
+// socket setup
+// we get the userID and receiverID from the database, will implement that part soon
+let users = {};  // is an object that maps userIds to their socket IDs.
+
+io.on('connection', (socket) => {  // listens for new socket connections.
+    console.log('A user connected');
+
+    socket.on('register', userId => {  // handles user registration by storing their socket ID.
+        users[userId] = socket.id;
+    });
+
+    socket.on('send_message', ({ senderId, receiverId, message }) => { //  handles sending messages from one user to another.
+        if (users[receiverId]) {
+            io.to(users[receiverId]).emit('new_message', { senderId, message });
+        }
+    });
+
+    socket.on('disconnect', () => {   //  removes the user's socket ID upon disconnection.
+        console.log('User disconnected');
+        for (let userId in users) {
+            if (users[userId] === socket.id) {
+                delete users[userId];
+                break;
+            }
+        }
+    });
+});
+// ----------------------------------------------------------------------------------------
 
 app.listen(port, () => {
     console.log(`litening st ${port}`)
