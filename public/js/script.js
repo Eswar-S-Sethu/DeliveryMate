37ebf62f98e75a817a5b01dde091eaee445625ca
lswitlearning <lswitlearning@gmail.com>
2024-01-21 00:10:55 +1100

    Create Rquest page without decoration.

--- public/js/script.js
+++ public/js/script.js
@@ -57,3 +57,43 @@ debugger
     event.preventDefault();
     sendData();
 });
+
+
+
+
+//create requests page~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function uploadImage() {
+    var fileInput = document.getElementById('fileInput');
+    var selectedFile = fileInput.files[0];
+    if (selectedFile) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var imageContainer = document.querySelector('.card-body[style="min-height: 200px;"]');
+            var imgElement = document.createElement('img');
+            imgElement.src = e.target.result;
+            imgElement.style.width = '100%'; 
+            imgElement.style.height = '100%'; 
+            imgElement.style.objectFit = 'cover'; 
+
+            imageContainer.innerHTML = '';
+            imageContainer.appendChild(imgElement);
+        };
+
+
+        reader.readAsDataURL(selectedFile);
+    }
+}
+
+function submit() {
+    var itemName = document.getElementById('inputName').value;
+    var itemWeight = document.getElementById('inputWeight').value;
+    var itemSize = document.getElementById('inputSize').value;
+    var itemDestination = document.getElementById('inputDestination').value;
+    var itemPickup = document.getElementById('inputpickup').value;
+    var itemTips = document.getElementById('inputtips').value;
+    var itemNotes = document.getElementById('inputnotes').value;
+
+    alert('Request submitted successfully!');
+}
+
+//create requests page end~~~~~~~~~~~~~~~~~~~~~~~~~
